f7f6977312051aa83ce764c6cc00e231d2194fb2
Larry Hill <tomella1@gmail.com>
2015-08-19 16:00:49 +1000

    Update draw to expose tool as a service.

--- source/map/draw/draw.js
+++ source/map/draw/draw.js
@@ -1,13 +1,13 @@
 /*!
  * Copyright 2015 Geoscience Australia (http://www.ga.gov.au/copyright.html)
  */
-(function(angular, L){
-	
+
+(function(angular){
 'use strict';
 
 angular.module("geo.draw", ['geo.map'])
 
-.directive("geoDraw", ['$log', '$rootScope', 'mapService', function($log, $rootScope, mapService) {
+.directive("geoDraw", ['$log', '$rootScope', 'drawService', function($log, $rootScope, drawService) {
 	var DEFAULTS = {
 		rectangleEvent : "geo.draw.rectangle.created",
 		lineEvent : "geo.draw.line.created"
@@ -22,46 +22,81 @@ angular.module("geo.draw", ['geo.map'])
 			lineEvent : "@"
 		},
 		link : function(scope, element, attrs, ctrl) {
+			
 			angular.forEach(DEFAULTS, function(value, key) {
 				if(!scope[key]) {
 					scope[key] = value;
 				}
 			});
-						
-			mapService.getMap().then(function(map) {
+			
+				
+			drawService.createControl(scope);
+		}
+	};
+}])
+
+.factory("drawService", ['$q', '$rootScope', 'mapService', function($q, $rootScope, mapService) {
+	var drawControl,
+		drawer,
+		featureGroup,
+		rectangleDeferred;	
+	
+	return {
+		createControl : function(parameters) {
+			if(drawControl) {
+				$q.when(drawControl);
+			}
+			
+			return mapService.getMap().then(function(map) {
 				var drawnItems = new L.FeatureGroup(),
-				    drawControl,
 				    options = { 
 				       edit: {
 				          featureGroup: drawnItems
 				       }
 				    };
 	
-				if(scope.data) {
-					angular.extend(options, scope.data);
-				}				
-				scope.drawnItems = drawnItems;
+				if(parameters.data) {
+					angular.extend(options, parameters.data);
+				}			
+				
+				featureGroup = parameters.drawnItems = drawnItems;
 				
 				map.addLayer(drawnItems);
 				// Initialise the draw control and pass it the FeatureGroup of editable layers
 				drawControl = new L.Control.Draw(options);
 				map.addControl(drawControl);
 				map.on("draw:created", function(event) {
-					scope.$apply(function() {
-						({
-							polyline : function() {
-								var data = {length:event.layer.getLength(), geometry:event.layer.getLatLngs()};
-								$rootScope.$broadcast(scope.lineEvent, data);
-							},
-							rectangle : function() {
-								$log.info("rect");
-							}
-						})[event.layerType]();
-					});
-				});					
+					({
+						polyline : function() {
+							var data = {length:event.layer.getLength(), geometry:event.layer.getLatLngs()};
+							$rootScope.$broadcast(parameters.lineEvent, data);
+						},
+						// With rectangles only one can be drawn at a time.
+						rectangle : function() {
+							var data = {bounds:event.layer.getBounds()};
+							rectangleDeferred.resolve(data);
+							rectangleDeferred = null;
+						}
+					})[event.layerType]();
+				});
+				
+				return drawControl;
 			});
+		},
+		
+		drawRectangle : function() {
+			if(rectangleDeferred) {
+				rectangleDeferred.reject();
+				rectangleDeferred = $q.defer();
+			} else {
+				rectangleDeferred = $q.defer();
+				mapService.getMap().then(function(map) {
+					drawer = new L.Draw.Rectangle(map, drawControl.options.polyline).enable();
+				});
+			}			
+			return rectangleDeferred.promise;
 		}
 	};
 }]);
 
-})(angular, L);
+})(angular);
